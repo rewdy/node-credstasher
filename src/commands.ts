87c0ef281e418d97759cefa4051b70dd1042a270
Andrew Meyer <andrewmeyer@granular.ag>
2025-07-14 16:02:44 -0500

    fix: include build in release

--- src/commands.ts
+++ src/commands.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import { CredstashClient } from "./lib/credstash";
+import { CredstashClient } from "./lib/credstash.js";
 import type {
   CommandOptions,
   DeleteCommandOptions,
